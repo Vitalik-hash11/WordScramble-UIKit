de6b6ef9914af2d34ffb395291a459ebad1318dc
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-08-09 22:57:02 +0300

    implement logic for the game

--- WordScramble-UIKit/ViewController.swift
+++ WordScramble-UIKit/ViewController.swift
@@ -50,8 +50,62 @@ class ViewController: UITableViewController {
     }
     
     private func submit(answer: String) {
-        usedWords.append(answer)
-        tableView.reloadData()
+        let lowercased = answer.lowercased()
+        
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowercased) {
+            if isOrigin(word: lowercased) {
+                if isReal(word: lowercased) {
+                    usedWords.append(lowercased)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Given word does not exist"
+                    errorMessage = "Use only real words!"
+                }
+            } else {
+                errorTitle = "You already used that word"
+                errorMessage = "Try to be more original"
+            }
+        } else {
+            errorTitle = "Word is not possible"
+            errorMessage = "You cannot spell that word from \(title!)"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    private func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else {return false}
+        
+        for letter in word {
+            if let possition = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: possition)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private func isOrigin(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    private func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
